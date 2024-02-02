0d05d8d53c2e497b310ad78bae6a792e045c9cd8
K Stephan Torres <kevinstephantorres@gmail.com>
2024-02-02 19:31:11 -0300

    Se instalo los iconos de bootstrap al proyecto

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,10 +1,11 @@
 import { Button, ListGroupItem } from "react-bootstrap";
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const TaskItem = ({taskName, deleteTask}) => {
     return (
         <ListGroupItem className="d-flex justify-content-between item">
             <span>{taskName}</span>
-            <Button variant="danger" onClick={()=>{deleteTask(taskName)}}>Borrar <i className="bi bi-trash-fill"></i></Button>            
+            <Button variant="danger" onClick={()=>{deleteTask(taskName)}}> Borrar <i className="bi bi-trash-fill"></i></Button>            
         </ListGroupItem>
     );
 };
